726b89e215e868ce6de08bef5234819fdca181f2
kobi <kobiokiti@gmail.com>
2026-01-17 16:21:24 +0100

    implemted the save favorites endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,30 @@
 import express from 'express';
 import { ENV } from './config/env.js';
+import { db } from './config/db.js';
+import { favotites } from './db/schema.js';
 
 const app = express();
 const PORT = ENV.PORT;
+
+app.use(express.json());
 
 app.get('/api/health', (req, res) => {
   res.status(200).json({success: true });
 });
 
+app.post("/api/favorites", async(req, res) => {
+  try {
+    const { userId, recipeId, title, image, cookTime, servings } = req.body;
+    if(!userId || !recipeId || !title) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+    const newFavorite = await db.insert(favotites).values({ userId, recipeId, title, image, cookTime, servings}).returning();
+    res.status(201).json(newFavorite);
+  } catch (error) {
+    console.error("Error adding favorite:", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
